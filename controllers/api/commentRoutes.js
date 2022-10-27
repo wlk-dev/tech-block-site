22ca781ea48682ebc8d1c179eb169142cfa58d94
wlk <william.koelling@gmail.com>
2022-10-26 20:40:54 -0500

    tested routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post: Comment } = require('../../models');
+const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth')
 
 // router.get('/:id', withAuth, (req, res) => {
