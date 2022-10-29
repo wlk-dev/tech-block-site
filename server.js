421b4b84347cec74ee4b46ce57e41d0a9f47fe53
wlk <william.koelling@gmail.com>
2022-10-28 20:40:57 -0500

    formatting

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -24,8 +26,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
