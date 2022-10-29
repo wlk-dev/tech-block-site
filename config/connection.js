5b4ce517a5cdc236b8dd67f57e10184e418e88c9
wlk <william.koelling@gmail.com>
2022-10-28 20:19:16 -0500

    setup for railway

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,20 @@ const Sequelize = require('sequelize');
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+let sequelize;
+if (process.env.MYSQL_URL) {
+    sequelize = new Sequelize(process.env.MYSQL_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
 
 module.exports = sequelize;
